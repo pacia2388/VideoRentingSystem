34113134c84021b88649a8b9d52e854991899563
pacia2388 <pacia2388@gmail.com>
2017-09-21 15:48:03 +0800

    Add project files.

---
+++ VideoRentingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoRentingSystem.Startup))]
+namespace VideoRentingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
