a9c77ef5f4266107a9805c019698a16540f8111c
pacia2388 <pacia2388@gmail.com>
2017-09-22 00:52:35 +0800

    Exercise 3

--- VideoRentingSystem/Controllers/MoviesController.cs
+++ VideoRentingSystem/Controllers/MoviesController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using VideoRentingSystem.Models;
 
@@ -9,20 +7,42 @@ namespace VideoRentingSystem.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
+        //        private List<Movie> GetMovies()
+        //        {
+        //            return new System.Collections.Generic.List<Movie>()
+        //            {
+        //                new Movie {Id = 1, Name = "Shrek"},
+        //                new Movie {Id = 2, Name = "Wall-e"}
+        //            };
+        //        }
 
-        private List<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new System.Collections.Generic.List<Movie>()
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
             {
-                new Movie {Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Wall-e"}
-            };
+                return HttpNotFound();
+            }
+
+            return View(movie);
         }
     }
 }
