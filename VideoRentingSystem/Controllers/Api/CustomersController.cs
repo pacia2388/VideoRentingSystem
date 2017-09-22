2e3852c0d0d3f01beebc3053b96d56670936beee
pacia2388 <pacia2388@gmail.com>
2017-09-23 00:09:00 +0800

    Implementing automapper to CustomerController

--- VideoRentingSystem/Controllers/Api/CustomersController.cs
+++ VideoRentingSystem/Controllers/Api/CustomersController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using AutoMapper;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
+using VideoRentingSystem.Dtos;
 using VideoRentingSystem.Models;
 
 namespace VideoRentingSystem.Controllers.Api
@@ -21,59 +23,57 @@ namespace VideoRentingSystem.Controllers.Api
         }
 
         // GET: /api/customers
-        public IEnumerable<Customer> GetCustomers()
+        public IEnumerable<CustomerDto> GetCustomers()
         {
-            return _context.Customers.ToList();
+            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
         }
 
         // GET: /api/customers/1
-        public Customer GetCustomer(int id)
+        public CustomerDto GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
 
-            return customer;
+            return Mapper.Map<Customer, CustomerDto>(customer);
         }
 
         // POST: /api/customers
         [HttpPost]
-        public Customer CreateCustomer(Customer customer)
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
             {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             _context.Customers.Add(customer);
             _context.SaveChanges();
 
-            return customer;
+            customerDto.Id = customer.Id;
+
+            return customerDto;
         }
 
         // PUT: /api/customers/1
         [HttpPut]
-        public Customer UpdateCustomer(int id, Customer customer)
+        public CustomerDto UpdateCustomer(int id, CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
-            {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
-            }
 
             var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customerInDb == null)
-            {
                 throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
 
-            customerInDb.Name = customer.Name;
-            customerInDb.BirthDate = customer.BirthDate;
-            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
-            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            //Mapper.Map<CustomerDto, Customer>(customerDto, customerInDb);
+            Mapper.Map(customerDto, customerInDb);
 
             _context.SaveChanges();
 
-            return customer;
+            return customerDto;
         }
 
         // DELETE: /api/customers/1
