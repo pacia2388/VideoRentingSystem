c795315638936c9a2463274e6835d8548048946e
pacia2388 <pacia2388@gmail.com>
2017-09-22 16:16:50 +0800

    Implement validation

--- VideoRentingSystem/Controllers/CustomerController.cs
+++ VideoRentingSystem/Controllers/CustomerController.cs
@@ -59,6 +59,15 @@ namespace VideoRentingSystem.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewmodel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewmodel);
+            }
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
