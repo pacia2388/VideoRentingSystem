a1fee131e8268bfe12f95f79a22d318c831727c9
pacia2388 <pacia2388@gmail.com>
2017-09-22 23:56:57 +0800

    set up Automapper

---
+++ VideoRentingSystem/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using VideoRentingSystem.Dtos;
+using VideoRentingSystem.Models;
+
+namespace VideoRentingSystem.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
