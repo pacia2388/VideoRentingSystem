ec030294a55ef77e953aa050d9707f231da3a128
pacia2388 <pacia2388@gmail.com>
2017-09-23 18:59:51 +0800

    Enable SSL and add a filter that requires https on the client

--- VideoRentingSystem/App_Start/FilterConfig.cs
+++ VideoRentingSystem/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace VideoRentingSystem
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
