00a33eab02a2a66aac362094e752de9f7124b656
pacia2388 <pacia2388@gmail.com>
2017-09-23 13:20:19 +0800

    Add jquery.datatables library and merge scripts bundle

--- VideoRentingSystem/App_Start/BundleConfig.cs
+++ VideoRentingSystem/App_Start/BundleConfig.cs
@@ -7,8 +7,16 @@ namespace VideoRentingSystem
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.js",
+                        "~/Scripts/respond.js"
+
+                ));
+            //            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -18,10 +26,10 @@ namespace VideoRentingSystem
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.js",
-                      "~/Scripts/respond.js"));
+            //            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //                      "~/Scripts/bootstrap.js",
+            //                      "~/Scripts/bootbox.js",
+            //                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
