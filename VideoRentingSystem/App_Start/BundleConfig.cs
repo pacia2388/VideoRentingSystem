4937cd51d464396f135f80c7e6eceb186f1d1db7
pacia2388 <pacia2388@gmail.com>
2017-09-23 13:27:51 +0800

    Add datatables bundles and implement jquery.datatables on customer table

--- VideoRentingSystem/App_Start/BundleConfig.cs
+++ VideoRentingSystem/App_Start/BundleConfig.cs
@@ -12,7 +12,9 @@ namespace VideoRentingSystem
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js",
                         "~/Scripts/bootbox.js",
-                        "~/Scripts/respond.js"
+                        "~/Scripts/respond.js",
+                        "~/Scripts/datatables/jquery.datatables.js",
+                        "~/Scripts/datatables/datatables.bootstrap.js"
 
                 ));
             //            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -33,6 +35,7 @@ namespace VideoRentingSystem
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
