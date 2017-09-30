c1e27d89e3ed802ef0a99e9a8d5624437f9304d5
pacia2388 <pacia2388@gmail.com>
2017-09-30 21:38:02 +0800

    using toastr

--- VideoRentingSystem/App_Start/BundleConfig.cs
+++ VideoRentingSystem/App_Start/BundleConfig.cs
@@ -15,11 +15,10 @@ namespace VideoRentingSystem
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.datatables.js",
                         "~/Scripts/datatables/datatables.bootstrap.js",
-                        "~/Scripts/typeahead.bundle.js"
+                        "~/Scripts/typeahead.bundle.js",
+                        "~/Scripts/toastr.js"
 
                 ));
-            //            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -29,15 +28,11 @@ namespace VideoRentingSystem
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            //            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //                      "~/Scripts/bootstrap.js",
-            //                      "~/Scripts/bootbox.js",
-            //                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
+                      "~/Content/toastr.css",
                       "~/Content/site.css"));
         }
     }
